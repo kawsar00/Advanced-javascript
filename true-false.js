26dc5905c5271365720f593484e875ae738a8447
kawsar00 <kawsarmahmud358@gmail.com>
2020-08-07 07:59:45 +0600

    true-false.js added

---
+++ true-false.js
@@ -0,0 +1,22 @@
+//falsy
+// 0
+// ""
+//false
+//undefined
+//null
+//NaN
+
+//truthy
+// " "
+//"0"
+//"false"
+// []
+// {}
+
+const name = 0;
+if (name) {
+  console.log('condition is true');
+}
+else{
+  console.log('condition is false')
+}
